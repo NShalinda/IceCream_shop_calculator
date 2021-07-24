2228c259934a7453c014588968f8d7ce397214c8
NShalinda <66541451+NShalinda@users.noreply.github.com>
2021-07-24 22:12:37 +0530

    first commit

---
+++ src/test/java/com/example/IceCoolCal/IceCoolCalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.IceCoolCal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IceCoolCalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
